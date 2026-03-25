f3c9de577805befa9d4a40794e14bfd2fe8d5c63
Alfred Kang <alfred@ctkpro.com>
2026-03-25 13:36:48 +0800

    更新了js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var heading = document.querySelector("h1");
     heading.addEventListener("click", function () {
         heading.textContent = "你點了標題！";
+        console.log('Good Job');
     });
 });
